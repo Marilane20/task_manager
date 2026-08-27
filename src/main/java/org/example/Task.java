cfa1fe144c5c9926c278c706f552115ae66c2f47
Marilane20 <marilanekamga@gmail.com>
2026-08-27 17:20:19 +0100

    feat: add task model class

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Task {
+
+    private final int id;
+    private String title;
+    private Boolean completed;
+
+
+
+    public Task(int id , String title){
+        this.id= id;
+        this.title = title;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Boolean isCompleted() {
+        return completed;
+    }
+
+    public  void setTitle( String title){
+        this.title = title;
+    }
+
+    public void MarkCompleted() {
+        this.completed = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +(completed? "x" : "" )  + "]" + id + ": " + title;
+    }
+}
